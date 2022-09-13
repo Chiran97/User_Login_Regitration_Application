954019070e70c63b7580b1a528dfac50884c8092
Chiran97 <chirandilanka@gmail.com>
2022-09-13 22:27:47 +0530

    Initial Commit

---
+++ src/main/java/com/example/user_login_regitration_application/UserLoginRegitrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_login_regitration_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginRegitrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginRegitrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/user_login_regitration_application/UserLoginRegitrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_login_regitration_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginRegitrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
